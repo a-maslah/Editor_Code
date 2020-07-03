7ef214d35fe08d7c7078f57195b60a343a96c2df
a-maslah <maslahamine20@gmail.com>
2020-07-03 02:21:31 +0100

    editor Code avec JS

---
+++ app.js
@@ -0,0 +1,22 @@
+let codes=document.getElementById("codes");
+let play=document.getElementById("play");
+let remove=document.getElementById("remove");
+let result=document.getElementById("result");
+
+
+play.onclick = () =>{
+   result.innerHTML = codes.value;
+
+localStorage.setItem("RESULT",codes.value);
+};
+
+remove.onclick =() =>{
+    result.innerHTML ="";
+    codes.value="";
+}
+
+onload = ()=>{
+    codes.value = localStorage.getItem("RESULT");
+    result.innerHTML = codes.value;
+
+}
